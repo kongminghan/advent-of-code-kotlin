b71fa09f84961321e3ae8e6457d795e1413ab825
Ming Han <minghan.kong@gmail.com>
2022-12-03 12:47:38 +0800

    Add solution for Day02 part 2

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -1,6 +1,7 @@
 package day2
 
 import readInput
+import java.lang.IllegalArgumentException
 
 const val wonScore = 6
 const val drawScore = 3
@@ -11,11 +12,22 @@ enum class Move(val score: Int) {
     Scissors(3)
 }
 
+data class Play(
+    val letter: String,
+    val move: Move
+)
+
 fun main() {
-    val winPairs = listOf(
+    val winPairs = mapOf(
         Move.Rock to Move.Paper,
         Move.Paper to Move.Scissors,
         Move.Scissors to Move.Rock
+    )
+
+    val loseMap = mapOf(
+        Move.Rock to Move.Scissors,
+        Move.Paper to Move.Rock,
+        Move.Scissors to Move.Paper
     )
 
     val moveMap = mapOf(
@@ -27,18 +39,38 @@ fun main() {
         "Z" to Move.Scissors,
     )
 
-    fun totalScore(input: List<String>): Int {
-        return input.sumOf {
-            it[0].code
-            val game = it.split(" ")
-            val pair = moveMap[game.first()]!! to moveMap[game.last()]!!
+    val Lose = "X"
+    val Draw = "Y"
+    val Win = "Z"
 
-            pair.second.score + if (winPairs.contains(pair)) {
+    fun List<String>.sumOfGame(transform: (Pair<Play, Play>) -> Int) = this.sumOf {
+        val game = it.split(" ")
+        val opponent = game.first()
+        val me = game.last()
+        transform(
+            Play(opponent, moveMap[opponent]!!) to Play(me, moveMap[me]!!)
+        )
+    }
+
+    fun totalScore(input: List<String>): Int {
+        return input.sumOfGame { pair ->
+            pair.second.move.score + if (winPairs[pair.first.move] == pair.second.move) {
                 wonScore
-            } else if (pair.first.score == pair.second.score) {
+            } else if (pair.first.move.score == pair.second.move.score) {
                 drawScore
             } else {
                 0
+            }
+        }
+    }
+
+    fun totalScoreWithoutGamePlay(input: List<String>): Int {
+        return input.sumOfGame {
+            when (it.second.letter) {
+                Lose -> loseMap[it.first.move]!!.score
+                Win -> winPairs[it.first.move]!!.score + wonScore
+                Draw -> it.first.move.score + drawScore
+                else -> throw IllegalArgumentException()
             }
         }
     }
@@ -48,13 +80,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return totalScore(input = input)
+        return totalScoreWithoutGamePlay(input = input)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(day = 2, name = "Day02_test")
     check(part1(testInput) == 19)
+    check(part2(testInput) == 16)
 
     val input = readInput(day = 2, name = "Day02")
     println(part1(input))
+    println(part2(input))
 }
