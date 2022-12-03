7ed06c946bb6e2816061250f1f73ee6df5ca0f80
Ming Han <minghan.kong@gmail.com>
2022-12-03 09:47:38 +0800

    Add solution for Day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun highestSumCalories(input: List<String>): Int {
+    fun highestSumCalories(input: List<String>, limit: Int): Int {
         return input
             .foldIndexed(mutableListOf<MutableList<Int>>(mutableListOf())) { _, acc, item ->
                 if (item == "") {
@@ -8,21 +8,28 @@ fun main() {
                     acc.last().add(item.toInt())
                 }
                 acc
-            }.maxOf { it.sum() }
+            }
+            .asSequence()
+            .map { it.sum() }
+            .sortedDescending()
+            .take(limit)
+            .sum()
     }
 
     fun part1(input: List<String>): Int {
-        return highestSumCalories(input)
+        return highestSumCalories(input, limit = 1)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return highestSumCalories(input = input, limit = 3)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 9)
+    check(part2(testInput) == 21)
 
     val input = readInput(name = "Day01")
     println(part1(input))
+    println(part2(input))
 }
