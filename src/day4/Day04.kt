f602900209712090778c161d407ded8c013ae581
Ming Han <minghan.kong@gmail.com>
2022-12-05 13:02:00 +0800

    Add solution for Day04 part 2

--- src/day4/Day04.kt
+++ src/day4/Day04.kt
@@ -1,6 +1,8 @@
 package day4
 
 import readInput
+import java.lang.Integer.max
+import java.lang.Integer.min
 
 fun main() {
 
@@ -12,31 +14,41 @@ fun main() {
     fun ClosedRange<Int>.containedIn(other: ClosedRange<Int>) =
         other.contains(this.start) && other.contains(this.endInclusive)
 
-    fun List<String>.findContainedRange(): List<Pair<IntRange, IntRange>> {
+    fun List<String>.findContainedRange(transform: (IntRange, IntRange) -> Pair<IntRange, IntRange>?): List<Pair<IntRange, IntRange>> {
         return this.mapNotNull {
             val (first, second) = it.split(",")
             val firstRange = first.toIntRange()
             val secondRange = second.toIntRange()
+            transform(firstRange, secondRange)
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.findContainedRange { firstRange, secondRange ->
             if (firstRange.containedIn(secondRange) || secondRange.containedIn(firstRange)) {
                 firstRange to secondRange
             } else {
                 null
             }
-        }
-    }
-
-    fun part1(input: List<String>): Int {
-        return input.findContainedRange().size
+        }.size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.findContainedRange { firstRange, secondRange ->
+            if ((firstRange.toSet() intersect secondRange.toSet()).isNotEmpty()) {
+                firstRange to secondRange
+            } else {
+                null
+            }
+        }.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(day = 4, name = "Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput(day = 4, name = "Day04")
     println(part1(input))
+    println(part2(input))
 }
